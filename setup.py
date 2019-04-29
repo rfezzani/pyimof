ca961faba0699e602537a6c1a4a7f93ef3cbfad9
rfezzani <rfezzani@gmail.com>
2019-04-30 00:34:02 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyimof',
+    version='0.0.0',
+    description="Python package for optical flow estimation",
+    package_dir={'pyimof': 'pyimof'},
+    package_data={'': ["data/*"]},
+    packages=find_packages(exclude=['test'])
+)
