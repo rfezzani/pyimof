7fea21a5ff85abe6b3ecc1030143e5726b62b6be
Riadh <r.fezzani@vitadx.com>
2019-04-30 15:35:14 +0200

    Add flow_to_color function

--- pyimof/util.py
+++ pyimof/util.py
@@ -5,6 +5,37 @@
 import numpy as np
 from scipy import ndimage as ndi
 from skimage.transform import pyramid_reduce, resize
+from skimage.color import hsv2rgb
+
+
+def forward_diff(p):
+    """Forward difference scheme
+
+    """
+    p_x = p.copy()
+    p_x[:, :-1] -= p[:, 1:]
+    p_x[:, -1] = p_x[:, -2]
+
+    p_y = p.copy()
+    p_y[:-1, :] -= p[1:, :]
+    p_y[-1, :] = p_y[-2, :]
+
+    return p_x, p_y
+
+
+def div(p1, p2):
+    """Divergence of P=(p1, p2) using backward differece scheme.
+
+    """
+    p1_x = p1.copy()
+    p1_x[:, 1:] -= p1[:, :-1]
+    p1_x[:, 0] = p1_x[:, 1]
+
+    p2_y = p2.copy()
+    p2_y[1:, :] -= p2[:-1, :]
+    p2_y[0, :] = p2_y[1, :]
+
+    return p1_x + p2_y
 
 
 def warp(I, u, v, x=None, y=None, mode='nearest'):
@@ -13,7 +44,7 @@ def warp(I, u, v, x=None, y=None, mode='nearest'):
     """
     if (x is None) or (y is None):
         nl, nc = I.shape
-        y, x = np.meshgrid(np.arange(nl), np.arange(nc), indexing='ij')
+        x, y = np.meshgrid(np.arange(nl), np.arange(nc), indexing='ij')
 
     return ndi.map_coordinates(I, [y+v, x+u], order=2, mode=mode)
 
@@ -24,12 +55,12 @@ def upscale_flow(u, v, shape):
     """
 
     nl, nc = u.shape
-    ax, ay = shape[0]/nl, shape[1]/nc
+    sx, sy = shape[0]/nl, shape[1]/nc
 
     u = resize(u, shape, preserve_range=True, anti_aliasing=False)
     v = resize(v, shape, preserve_range=True, anti_aliasing=False)
 
-    return ax*u, ay*v
+    return sx*u, sy*v
 
 
 def get_pyramid(I0, I1, downscale=2, min_size=16):
@@ -72,3 +103,17 @@ def coarse_to_fine(I0, I1, solver, downscale=2):
         u, v = solver(J0, J1, u, v)
 
     return u, v
+
+
+def flow_to_color(u, v):
+    N = np.sqrt(u*u + v*v)
+
+    u /= N
+    v /= N
+    N /= N.max()
+
+    hsv = np.concatenate([np.atleast_3d(u),
+                          np.atleast_3d(v),
+                          np.atleast_3d(N)], -1)
+
+    return hsv2rgb(hsv)
