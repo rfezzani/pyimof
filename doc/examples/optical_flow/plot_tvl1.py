872a52aa8ebaf48689be8d285c0cf527b7da0b11
rfezzani <rfezzani@gmail.com>
2019-05-04 00:25:34 +0200

    Add ilk to demo axample

--- doc/examples/optical_flow/plot_tvl1.py
+++ doc/examples/optical_flow/plot_tvl1.py
@@ -2,16 +2,14 @@ from time import time
 import matplotlib.pyplot as plt
 import pyimof
 
-I0, I1 = pyimof.data.rubberwhale
+fig = plt.figure(figsize=((8, 7)))
+ax1, ax2, ax3, ax4 = fig.subplots(2, 2).ravel()
+
+I0, I1 = pyimof.data.hydrangea
 
 t0 = time()
-u, v = pyimof.solvers.ilk(I0, I1)
+u, v = pyimof.solvers.tvl1(I0, I1)
 t1 = time()
-
-print("Processing time: {:02f}sec".format(t1-t0))
-
-fig = plt.figure(figsize=((5, 7)))
-ax1, ax2 = fig.subplots(2, 1)
 
 pyimof.display.quiver(u, v, ax=ax1)
 
@@ -19,6 +17,21 @@ img = pyimof.display.flow_to_color(u, v)
 
 ax2.imshow(img)
 ax2.set_axis_off()
+
+print("TV-L1 processing time: {:02f}sec".format(t1-t0))
+
+t0 = time()
+u, v = pyimof.solvers.ilk(I0, I1)
+t1 = time()
+
+pyimof.display.quiver(u, v, ax=ax3)
+
+img = pyimof.display.flow_to_color(u, v)
+
+ax4.imshow(img)
+ax4.set_axis_off()
+
+print("ILK processing time: {:02f}sec".format(t1-t0))
 
 plt.tight_layout()
 plt.show()
