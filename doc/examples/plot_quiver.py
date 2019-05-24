8b5492635474aa81268841d014c6b21113f6ed4d
Riadh <r.fezzani@vitadx.com>
2019-05-24 14:39:35 +0200

    Add title to figures

--- doc/examples/plot_quiver.py
+++ doc/examples/plot_quiver.py
@@ -32,11 +32,13 @@ ax0.set_axis_off()
 ax0.set_title("Reference image")
 
 pyimof.display.quiver(u, v, ax=ax1)
+ax1.set_title("Raw arrows")
 
 pyimof.display.quiver(u, v, c=norm, bg=I0, ax=ax2,
                       cmap='jet', bg_cmap='gray')
+ax2.set_title("Arrows colored according to their norm over Image")
 
-pyimof.display.quiver(u, v, c=norm, bg=norm, ax=ax3,
-                      cmap='Greys', bg_cmap='viridis')
+pyimof.display.quiver(u, v, bg=norm, ax=ax3, color='r')
+ax3.set_title("Red arrows over magnitude")
 
 plt.show()
